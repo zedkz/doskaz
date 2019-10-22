9da202786be25415885c54af4b1818dae02f858c
PetrGrishin <petro11@yandex.ru>
2019-10-22 07:40:24 +0300

    верстка маркера на карте

--- frontend/src/components/MainMap.vue
+++ frontend/src/components/MainMap.vue
@@ -1,9 +1,11 @@
 <template>
-  <yandexMap class="ymap" :settings="settings" :coords="coords" :zoom="zoom" :controls="controls" />
+  <yandexMap class="ymap" :settings="settings" :coords="coords" :zoom="zoom" :controls="controls">
+    <ymap-marker marker-id="123" :coords="markerCoords" :icon="markerIcon" />
+  </yandexMap>
 </template>
 
 <script>
-import { yandexMap } from "vue-yandex-maps";
+import { yandexMap, ymapMarker } from "vue-yandex-maps";
 
 export default {
   data() {
@@ -16,10 +18,25 @@ export default {
       },
       coords: [51.159807, 71.438674],
       zoom: 15,
-      controls: ["zoomControl"]
+      controls: [],
+
+      markerCoords: [51.159807, 71.446774],
+      markerIcon: {
+        layout: "default#imageWithContent",
+        imageHref: "",
+        imageSize: [50, 61],
+        imageOffset: [0, 0],
+        content:
+          "<img src='" +
+          require("@/assets/icons/bar.svg") +
+          "' style='max-width: 27px; max-height: 27px; z-index: 1;'/>",
+        contentOffset: [0, 0],
+        contentLayout:
+          '<div style="border: none; position: relative; display: flex; width: 50px; height: 61px; padding-bottom: 11px; justify-content: center; align-items: center;"><svg width="50" height="61" viewBox="0 0 50 61" fill="none" xmlns="http://www.w3.org/2000/svg" style="position: absolute; top: 0; left: 0; z-index: 0;"><path d="M50 0H0V50H14.6667L25 60.3333L35.3333 50H50V0Z" fill="#F8AC1A"/></svg>$[properties.iconContent]</div>'
+      }
     };
   },
-  components: { yandexMap }
+  components: { yandexMap, ymapMarker }
 };
 </script>
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: 'http://localhost:8000'
-            }
-        }
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://doskaz.vps3.zed.kz"
+      }
     }
+  }
 };
