240debbcf9d3ce496eb498c14c5242928723aad5
smolin <smolin9000@gmail.com>
2020-01-07 17:45:38 +0600

    Complaint validate

--- backend/src/Complaints/ComplaintController.php
+++ backend/src/Complaints/ComplaintController.php
@@ -41,6 +41,8 @@ final class ComplaintController extends AbstractController
 
     /**
      * @Route(path="/authorities", methods={"GET"})
+     * @param Connection $connection
+     * @return mixed[]
      */
     public function complaintAuthorities(Connection $connection)
     {
@@ -158,7 +160,6 @@ final class ComplaintController extends AbstractController
 
         return (new BinaryFileResponse($path, 200, [], true))->deleteFileAfterSend();
     }
-
 
     public function complaintAttributes()
     {
@@ -440,5 +441,13 @@ final class ComplaintController extends AbstractController
                 ]
             ]
         ];
+    }
+
+    /**
+     * @Route(path="/validate", methods={"POST"})
+     * @param ComplaintData $complaintData
+     */
+    public function validate(ComplaintData $complaintData) {
+
     }
 }
