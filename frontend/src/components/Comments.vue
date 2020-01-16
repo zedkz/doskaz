8df4ea18496fed146d764a5419b055dc493fca64
Oksnard <jackypyatochka@gmail.com>
2020-01-16 14:22:38 +0300

    Create custom comments block

---
+++ frontend/src/components/Comments.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="comments-block">
+    <h4>{{ declension }}</h4>
+    <div class="comments-list">
+      <div class="person-comment" v-for="comment in comments" :key="comment.id">
+        <div>
+          <img
+            :src="comment.userAvatar"
+            class="avatar"
+            v-if="comment.userAvatar.length !== 0"
+          />
+          <div
+            v-else
+            :style="`background:${colorGenerator()}`"
+            class="no-avatar"
+          ></div>
+        </div>
+        <div class="person-info">
+          <div>
+            <span class="person-name">{{ comment.userName }}</span>
+            <p class="text-comment">{{ comment.text }}</p>
+            <span class="date-comment small">Вчера в 22:25</span>
+            <span class="small" @click="sendComment(comment.id)">Ответить</span>
+          </div>
+          <div
+            class="replie-comment"
+            v-for="replie in comment.replies"
+            :key="replie.id"
+          >
+            <div>
+              <img
+                :src="replie.userAvatar"
+                class="avatar"
+                v-if="replie.userAvatar.length !== 0"
+              />
+              <div
+                v-else
+                :style="`background:${colorGenerator()}`"
+                class="no-avatar"
+              ></div>
+            </div>
+            <div class="person-info">
+              <span class="person-name">{{ replie.userName }}</span
+              ><small class="small">
+                ответил {{ serchParenById(replie.parentId).userName }}</small
+              >
+              <p class="text-comment">{{ replie.text }}</p>
+              <span class="date-comment small">Вчера в 22:25</span>
+              <span class="small" @click="sendComment(replie.id)"
+                >Ответить</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <form>
+        <textarea
+          ref="comment"
+          name="comment"
+          id="1"
+          rows="2"
+          v-model="comment"
+          @input="resizeHeight"
+        />
+        <div class="form-actions">
+          <div>
+            <button type="button" class="send-button" @click="sendComment()">
+              <img src="@/assets/icons/send.svg" alt="" />
+            </button>
+            <button
+              type="button"
+              @click="clearComment()"
+              class="clear-button"
+              v-if="comment.length > 0"
+            >
+              <img src="@/assets/icons/close.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </form>
+      <p class="comment-error" v-if="commentError">Пожалуйста укажите комментарий</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "@/api";
+
+export default {
+  props: ["id", "postSlug"],
+  data: () => ({
+    comment: "",
+    postId: "",
+    comments: [],
+    commentError: false
+  }),
+  mounted() {
+    api
+      .get(`blogPosts/bySlug/${this.$route.params.postSlug}`)
+      .then(response => {
+        this.postId = response.data.post.id;
+        api.get(`blogPosts/${this.postId}/comments`).then(res => {
+          this.comments = res.data.items;
+          this;
+        });
+      });
+  },
+  computed: {
+    declension() {
+      let number = this.comments.length;
+      let txt = ["Комментарий", "Комментария", "Комментариев"];
+      let cases = [2, 0, 1, 1, 1, 2];
+      return (
+        number +
+        " " +
+        txt[
+          number % 100 > 4 && number % 100 < 20
+            ? 2
+            : cases[number % 10 < 5 ? number % 10 : 5]
+        ]
+      );
+    }
+  },
+  methods: {
+    resizeHeight(e) {
+      this.commentError = false
+      e.target.style.height = "70px";
+      e.target.style.height = e.target.scrollHeight + "px";
+    },
+    sendComment(id) {
+      if (id == undefined) {
+        id = null
+      }
+
+      if (this.comment.length > 1) {
+        api.post(`blogPosts/${this.postId}/comments`, {
+          text: this.comment,
+          parentId: id
+        })
+        .then(res => {
+          console.log(res);
+        });
+      } else {
+        this.commentError = true
+      }
+    },
+    clearComment() {
+      this.comment = "";
+    },
+    colorGenerator() {
+      return "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+    },
+    serchParenById(id) {
+      return this.comments.find(comment => comment.id === id);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.comments-block {
+  h4 {
+    font-size: 22px;
+    line-height: 30px;
+    margin: 27px 0 17px 0;
+  }
+  .replie-comment {
+    margin-top: 29px;
+  }
+  .person-comment,
+  .replie-comment {
+    display: flex;
+    margin-bottom: 35px;
+
+    .person-info {
+      padding-top: 6px;
+      margin-left: 10px;
+
+      .person-name {
+        font-weight: bold;
+        font-size: 16px;
+      }
+
+      .date-comment {
+        margin-right: 40px;
+      }
+
+      .small {
+        color: #5b6067;
+        font-size: 14px;
+      }
+    }
+    .no-avatar {
+      height: 30px;
+      width: 30px;
+      border-radius: 50%;
+    }
+  }
+
+  form {
+    display: flex;
+
+    textarea {
+      padding: 14px 20px;
+      width: 100%;
+      border: 1px solid #7b95a7;
+      box-sizing: border-box;
+      resize: none;
+      overflow: hidden;
+      height: 100%;
+    }
+
+    .form-actions {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      button {
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        outline: none;
+        background: none;
+      }
+    }
+  }
+
+  .comment-error {
+    color: red;
+  }
+}
+</style>
--- frontend/src/components/blog/BlogInside.vue
+++ frontend/src/components/blog/BlogInside.vue
@@ -1,344 +1,372 @@
 <template>
-    <div class="blog__inside">
-        <div class="breadcrumbs">
-            <router-link :to="{name: 'blog'}" class="breadcrumbs__link">Блог</router-link>
-            <router-link :to="{name: 'blog', params: {categorySlug: $route.params.categorySlug}}"
-                         class="breadcrumbs__link">{{ post.categoryTitle }}
-            </router-link>
-        </div>
-        <div class="blog__in">
-            <div class="blog__content">
-                <div class="blog__inside-content">
-                    <h3>{{ post.title }}</h3>
-                    <img :src="post.image" :alt="post.title"/>
-                    <div v-html="post.content">
-                    </div>
-                    <div class="blog__inside-bottom">
-                        <span class="blog__inside-date" v-if="post.id">{{ post.publishedAt | date }}</span>
-                        <div class="social --md">
-                            <a class="social__link --fcb" @click="share('fb')">
-                                <img src="@/assets/img/social/fcb.svg"/>
-                            </a>
-                            <a class="social__link --vk" @click="share('vk')">
-                                <img src="@/assets/img/social/vk.svg"/>
-                            </a>
-                            <a class="social__link --ok" @click="share('ok')">
-                                <img src="@/assets/img/social/ok.svg"/>
-                            </a>
-                            <a class="social__link --my" @click="share('mailru')">
-                                <img src="@/assets/img/social/my.svg"/>
-                            </a>
-                        </div>
-                    </div>
-                    <h4>Комментарии</h4>
-                    <vue-disqus shortname="pavlodarzedkz" :title="post.title" :key="post.id"/>
-                </div>
-            </div>
-            <div class="blog__side">
-                <div class="blog__category">
-                    <span class="blog__category-title">Похожие материалы</span>
-                    <ul class="blog__material">
-                        <li class="blog__material-item" v-for="post in similarPosts" :key="post.id">
-                            <router-link
-                                    :to="{name: 'blogView', params: {categorySlug: post.categorySlug, postSlug: post.slug}}"
-                                    class="blog__material-link">
-                                <img :src="post.image" :alt="post.title"/>
-                                <span>{{ post.title }}</span>
-                            </router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+  <div class="blog__inside">
+    <div class="breadcrumbs">
+      <router-link :to="{ name: 'blog' }" class="breadcrumbs__link"
+        >Блог</router-link
+      >
+      <router-link
+        :to="{
+          name: 'blog',
+          params: { categorySlug: $route.params.categorySlug }
+        }"
+        class="breadcrumbs__link"
+        >{{ post.categoryTitle }}
+      </router-link>
     </div>
+    <div class="blog__in">
+      <div class="blog__content">
+        <div class="blog__inside-content">
+          <h3>{{ post.title }}</h3>
+          <img :src="post.image" :alt="post.title" />
+          <div v-html="post.content"></div>
+          <div class="blog__inside-bottom">
+            <span class="blog__inside-date" v-if="post.id">{{
+              post.publishedAt | date
+            }}</span>
+            <div class="social --md">
+              <a class="social__link --fcb" @click="share('fb')">
+                <img src="@/assets/img/social/fcb.svg" />
+              </a>
+              <a class="social__link --vk" @click="share('vk')">
+                <img src="@/assets/img/social/vk.svg" />
+              </a>
+              <a class="social__link --ok" @click="share('ok')">
+                <img src="@/assets/img/social/ok.svg" />
+              </a>
+              <a class="social__link --my" @click="share('mailru')">
+                <img src="@/assets/img/social/my.svg" />
+              </a>
+            </div>
+          </div>
+          
+          <!-- <vue-disqus shortname="pavlodarzedkz" :title="post.title" :key="post.id"/> -->
+          <Comments :id="post.id" :postSlug="this.$route.params.postSlug"/>
+        </div>
+      </div>
+      <div class="blog__side">
+        <div class="blog__category">
+          <span class="blog__category-title">Похожие материалы</span>
+          <ul class="blog__material">
+            <li
+              class="blog__material-item"
+              v-for="post in similarPosts"
+              :key="post.id"
+            >
+              <router-link
+                :to="{
+                  name: 'blogView',
+                  params: {
+                    categorySlug: post.categorySlug,
+                    postSlug: post.slug
+                  }
+                }"
+                class="blog__material-link"
+              >
+                <img :src="post.image" :alt="post.title" />
+                <span>{{ post.title }}</span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Vue from 'vue'
-    import VueDisqus from 'vue-disqus'
-    import api from '@/api'
-    import get from 'lodash/get'
-    import {format} from 'date-fns'
-    import {ru} from 'date-fns/locale'
+import Vue from "vue";
+import VueDisqus from "vue-disqus";
+import Comments from "@/components/Comments";
+import api from "@/api";
+import get from "lodash/get";
+import { format } from "date-fns";
+import { ru } from "date-fns/locale";
 
-    Vue.use(VueDisqus);
+Vue.use(VueDisqus);
 
-    export default {
-        metaInfo() {
-            return {
-                title: get(this.post, 'meta.title'),
-                meta: [
-                    {name: 'keywords', content: get(this.post, 'meta.keywords')},
-                    {name: 'description', content: get(this.post, 'meta.description')},
-                    {property: 'og:title', content: get(this.post, 'meta.ogTitle')},
-                    {property: 'og:description', content: get(this.post, 'meta.ogDescription')},
-                    {property: 'og:image', content: get(this.post, 'meta.ogImage')}
-                ].filter(({content}) => !!content)
-            }
+export default {
+  components: { Comments },
+  metaInfo() {
+    return {
+      title: get(this.post, "meta.title"),
+      meta: [
+        { name: "keywords", content: get(this.post, "meta.keywords") },
+        { name: "description", content: get(this.post, "meta.description") },
+        { property: "og:title", content: get(this.post, "meta.ogTitle") },
+        {
+          property: "og:description",
+          content: get(this.post, "meta.ogDescription")
         },
-        mounted() {
-            console.log(this.$route)
-        },
-        data() {
-            return {
-                post: {},
-                similarPosts: []
-            }
-        },
-        filters: {
-            date(value) {
-                return format(new Date(value), 'd MMMM y', {locale: ru})
-            }
-        },
-        watch: {
-            '$route': {
-                handler() {
-                    this.loadPost();
-                },
-                immediate: true
-            }
-        },
-        methods: {
-            async loadPost() {
-                const {data: {post, similar}} = await api.get(`blogPosts/bySlug/${this.$route.params.postSlug}`);
-                this.post = post;
-                this.similarPosts = similar
-            },
-            share(network) {
-                window.open(this.shareLinks[network])
-            }
-        },
-        computed: {
-            shareLinks() {
-                const path = encodeURIComponent(window.location.origin + this.$route.fullPath);
-
-                return {
-                    ok: `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${path}`,
-                    fb: `https://www.facebook.com/sharer.php?u=${path}`,
-                    vk: `https://vk.com/share.php?url=${path}`,
-                    mailru: `https://connect.mail.ru/share?url=${path}`,
-                }
-            }
-        }
+        { property: "og:image", content: get(this.post, "meta.ogImage") }
+      ].filter(({ content }) => !!content)
+    };
+  },
+  mounted() {
+    console.log(this.$route);
+  },
+  data() {
+    return {
+      post: {},
+      similarPosts: []
+    };
+  },
+  filters: {
+    date(value) {
+      return format(new Date(value), "d MMMM y", { locale: ru });
     }
+  },
+  watch: {
+    $route: {
+      handler() {
+        this.loadPost();
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    async loadPost() {
+      const {
+        data: { post, similar }
+      } = await api.get(`blogPosts/bySlug/${this.$route.params.postSlug}`);
+      this.post = post;
+      this.similarPosts = similar;
+    },
+    share(network) {
+      window.open(this.shareLinks[network]);
+    }
+  },
+  computed: {
+    shareLinks() {
+      const path = encodeURIComponent(
+        window.location.origin + this.$route.fullPath
+      );
 
+      return {
+        ok: `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${path}`,
+        fb: `https://www.facebook.com/sharer.php?u=${path}`,
+        vk: `https://vk.com/share.php?url=${path}`,
+        mailru: `https://connect.mail.ru/share?url=${path}`
+      };
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-    .slider {
-        position: relative;
-        padding: 0 20px;
-        margin: 30px 0 70px;
+.slider {
+  position: relative;
+  padding: 0 20px;
+  margin: 30px 0 70px;
 
-        img {
-            margin: 0 auto;
-        }
+  img {
+    margin: 0 auto;
+  }
 
-        .slick-prev {
+  .slick-prev {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 20px;
+    height: 50px;
+    margin: -25px 0 0;
+    cursor: pointer;
+  }
+
+  .slick-next {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    width: 20px;
+    height: 50px;
+    margin: -25px 0 0;
+    cursor: pointer;
+  }
+}
+
+.blog {
+  &__inside {
+    padding: 35px 0 40px;
+
+    &-content {
+      > h3 {
+        font-size: 32px;
+        line-height: 40px;
+        margin: 0 0 20px;
+      }
+
+      > h4 {
+        font-size: 22px;
+        line-height: 30px;
+        margin: 0 0 18px 0;
+      }
+
+      > p {
+        font-size: 16px;
+        line-height: 30px;
+        margin: 20px 0;
+      }
+
+      > img {
+        max-width: 100%;
+        margin: 32px 0 28px;
+        display: block;
+      }
+
+      > ul {
+        padding: 0 0 0 40px;
+        margin: -10px 0 20px;
+        list-style: none;
+
+        li {
+          position: relative;
+          font-size: 16px;
+          line-height: 30px;
+          margin: 10px 0 0;
+
+          &:first-child {
+            margin: 0;
+          }
+
+          &:before {
             position: absolute;
-            left: 0;
-            top: 50%;
-            width: 20px;
-            height: 50px;
-            margin: -25px 0 0;
-            cursor: pointer;
+            left: -18px;
+            top: 12px;
+            content: "";
+            width: 6px;
+            height: 6px;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            border-radius: 50%;
+            background: #0f6bf5;
+          }
         }
+      }
 
-        .slick-next {
-            position: absolute;
-            right: 0;
-            top: 50%;
-            width: 20px;
-            height: 50px;
-            margin: -25px 0 0;
-            cursor: pointer;
-        }
-    }
-
-    .blog {
-        &__inside {
-            padding: 35px 0 40px;
-
-            &-content {
-                > h3 {
-                    font-size: 32px;
-                    line-height: 40px;
-                    margin: 0 0 20px;
-                }
-
-                > h4 {
-                    font-size: 22px;
-                    line-height: 30px;
-                    margin: 0 0 18px 0;
-                }
-
-                > p {
-                    font-size: 16px;
-                    line-height: 30px;
-                    margin: 20px 0;
-                }
-
-                > img {
-                    max-width: 100%;
-                    margin: 32px 0 28px;
-                    display: block;
-                }
-
-                > ul {
-                    padding: 0 0 0 40px;
-                    margin: -10px 0 20px;
-                    list-style: none;
-
-                    li {
-                        position: relative;
-                        font-size: 16px;
-                        line-height: 30px;
-                        margin: 10px 0 0;
-
-                        &:first-child {
-                            margin: 0;
-                        }
-
-                        &:before {
-                            position: absolute;
-                            left: -18px;
-                            top: 12px;
-                            content: '';
-                            width: 6px;
-                            height: 6px;
-                            -webkit-border-radius: 50%;
-                            -moz-border-radius: 50%;
-                            border-radius: 50%;
-                            background: #0F6BF5;
-                        }
-                    }
-                }
-
-                > a {
-                    display: flex;
-                    align-items: center;
-                    padding: 20px 40px;
-                    background: #D5E9FC;
-                    font-size: 0;
-                    transition: opacity 0.3s;
-
-                    &:hover {
-                        opacity: 0.7;
-                    }
-
-                    svg {
-                        display: inline-block;
-                        vertical-align: middle;
-                    }
-
-                    div {
-                        text-align: center;
-                        width: 30px;
-                        height: 30px;
-                        line-height: 30px;
-                        background: #0F6BF5;
-                        margin: 0 10px 0 0;
-                    }
-
-                    span {
-                        font-size: 16px;
-                        line-height: 30px;
-                        color: #333333;
-                    }
-                }
-            }
-
-            &-pic {
-                display: flex;
-                flex-wrap: wrap;
-                align-items: flex-start;
-                justify-content: flex-start;
-                margin: -5px -15px;
-
-                img {
-                    margin: 15px;
-                }
-            }
-
-            &-bottom {
-                margin: 30px 0 28px;
-                padding: 0 0 40px;
-                border-bottom: 1px solid #7B95A7;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-            }
-
-            &-date {
-                color: #5B6067;
-                font-size: 14px;
-                line-height: 20px;
-            }
-
-            .blog__in {
-                padding: 32px 0 0;
-            }
-        }
-
-        &__material {
-            list-style: none;
-            padding: 0;
-
-            &-item {
-                margin: 30px 0 0;
-            }
-
-            &-link {
-                display: block;
-                transition: opacity 0.3s;
-
-                &:hover {
-                    opacity: 0.7;
-                }
-
-                img {
-                    display: block;
-                    max-width: 100%;
-                    margin: 0 0 15px;
-                }
-
-                span {
-                    display: block;
-                    font-size: 14px;
-                    line-height: 20px;
-                    color: #333333;
-                }
-            }
-        }
-    }
-
-    .breadcrumbs {
+      > a {
+        display: flex;
+        align-items: center;
+        padding: 20px 40px;
+        background: #d5e9fc;
         font-size: 0;
+        transition: opacity 0.3s;
 
-        &__link {
-            display: inline-block;
-            vertical-align: middle;
-            font-size: 14px;
-            line-height: 20px;
-            color: #5B6067;
-            transition: opacity 0.3s;
-
-            &:hover {
-                opacity: 0.7
-            }
-
-            &:before {
-                content: '/';
-                display: inline-block;
-                vertical-align: middle;
-                margin: 0 5px;
-            }
-
-            &:first-child {
-                &:before {
-                    display: none;
-                }
-            }
+        &:hover {
+          opacity: 0.7;
         }
+
+        svg {
+          display: inline-block;
+          vertical-align: middle;
+        }
+
+        div {
+          text-align: center;
+          width: 30px;
+          height: 30px;
+          line-height: 30px;
+          background: #0f6bf5;
+          margin: 0 10px 0 0;
+        }
+
+        span {
+          font-size: 16px;
+          line-height: 30px;
+          color: #333333;
+        }
+      }
     }
+
+    &-pic {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: flex-start;
+      justify-content: flex-start;
+      margin: -5px -15px;
+
+      img {
+        margin: 15px;
+      }
+    }
+
+    &-bottom {
+      margin: 30px 0 28px;
+      padding: 0 0 40px;
+      border-bottom: 1px solid #7b95a7;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    &-date {
+      color: #5b6067;
+      font-size: 14px;
+      line-height: 20px;
+    }
+
+    .blog__in {
+      padding: 32px 0 0;
+    }
+  }
+
+  &__material {
+    list-style: none;
+    padding: 0;
+
+    &-item {
+      margin: 30px 0 0;
+    }
+
+    &-link {
+      display: block;
+      transition: opacity 0.3s;
+
+      &:hover {
+        opacity: 0.7;
+      }
+
+      img {
+        display: block;
+        max-width: 100%;
+        margin: 0 0 15px;
+      }
+
+      span {
+        display: block;
+        font-size: 14px;
+        line-height: 20px;
+        color: #333333;
+      }
+    }
+  }
+}
+
+.breadcrumbs {
+  font-size: 0;
+
+  &__link {
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 14px;
+    line-height: 20px;
+    color: #5b6067;
+    transition: opacity 0.3s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    &:before {
+      content: "/";
+      display: inline-block;
+      vertical-align: middle;
+      margin: 0 5px;
+    }
+
+    &:first-child {
+      &:before {
+        display: none;
+      }
+    }
+  }
+}
 </style>
