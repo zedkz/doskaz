9729daf90e8e327e4aba6ab25d7b082bd2da3324
smolin <smolin9000@gmail.com>
2020-01-21 11:11:27 +0600

    Objects categories filtering

--- backend/src/Objects/ObjectsApiController.php
+++ backend/src/Objects/ObjectsApiController.php
@@ -70,56 +70,70 @@ final class ObjectsApiController extends AbstractController
 
         $precision = $clusteringLevels[$zoom] ?? 11;
 
-        $query = "       
-           SELECT COUNT(*) AS number,
-                   ST_GEOHASH(point_value, :precision) AS hash,
-                   ST_XMIN(ST_COLLECT(objects.point_value::GEOMETRY)) AS p1x,
-                   ST_YMIN(ST_COLLECT(objects.point_value::GEOMETRY)) AS p1y,
-                   ST_XMAX(ST_COLLECT(objects.point_value::GEOMETRY)) AS p2x,
-                   ST_YMAX(ST_COLLECT(objects.point_value::GEOMETRY)) AS p2y,
-                   ST_X(ST_CENTROID(ST_COLLECT(objects.point_value::GEOMETRY))) AS lat,
-                   ST_Y(ST_CENTROID(ST_COLLECT(objects.point_value::GEOMETRY))) AS long
-            FROM objects
-            WHERE ST_CONTAINS(ST_MAKEENVELOPE(:x1,:y1,:x2,:y2, 4326), point_value::GEOMETRY)
-            GROUP BY hash
-            HAVING COUNT(*) > 1
-        ";
-
-        $clusters = [];
-
-        if ($zoom < 19) {
-            $clusters = $connection->executeQuery($query, [
+        $q1 = $connection->createQueryBuilder()
+            ->select([
+                'COUNT(*) AS number',
+                'ST_GEOHASH(point_value, :precision) AS hash',
+                'ST_XMIN(ST_COLLECT(objects.point_value::GEOMETRY)) AS p1x',
+                'ST_YMIN(ST_COLLECT(objects.point_value::GEOMETRY)) AS p1y',
+                'ST_XMAX(ST_COLLECT(objects.point_value::GEOMETRY)) AS p2x',
+                'ST_YMAX(ST_COLLECT(objects.point_value::GEOMETRY)) AS p2y',
+                'ST_X(ST_CENTROID(ST_COLLECT(objects.point_value::GEOMETRY))) AS lat',
+                'ST_Y(ST_CENTROID(ST_COLLECT(objects.point_value::GEOMETRY))) AS long'
+            ])
+            ->from('objects')
+            ->andWhere('ST_CONTAINS(ST_MAKEENVELOPE(:x1,:y1,:x2,:y2, 4326), point_value::GEOMETRY)')
+            ->groupBy('hash')
+            ->having('COUNT(*) > 1')
+            ->setParameters([
                 'x1' => $boundary[0],
                 'y1' => $boundary[1],
                 'x2' => $boundary[2],
                 'y2' => $boundary[3],
                 'precision' => $precision
-            ])->fetchAll();
+            ]);
+
+        $categories = $request->query->get('categories', []);
+        if (count($categories)) {
+            $q1->andWhere('category_id IN (:categories)')
+                ->setParameter('categories', $categories, Connection::PARAM_STR_ARRAY);
         }
 
+        $clusters = [];
+
+        if ($zoom < 19) {
+            $clusters = $q1->execute()->fetchAll();
+        }
 
         $ids = array_column($clusters, 'hash');
 
-        $query2 = "
-            SELECT
-               *,
-               ST_X(ST_AsText(point_value)) as lat,
-               ST_Y(ST_AsText(point_value)) as long
-            FROM objects 
-            WHERE ST_CONTAINS(ST_MAKEENVELOPE(:x1,:y1,:x2,:y2, 4326), point_value::GEOMETRY)
-            and st_geohash(point_value, :precision) not in (:ids)
-        ";
+        $q2 = $connection->createQueryBuilder()
+            ->select([
+                '*',
+                'ST_X(ST_AsText(point_value)) as lat',
+                'ST_Y(ST_AsText(point_value)) as long'
+            ])
+            ->from('objects')
+            ->andWhere('ST_CONTAINS(ST_MAKEENVELOPE(:x1,:y1,:x2,:y2, 4326), point_value::GEOMETRY)')
+            ->andWhere('ST_GEOHASH(point_value, :precision) NOT IN (:ids)')
+            ->setParameters([
+                'x1' => $boundary[0],
+                'y1' => $boundary[1],
+                'x2' => $boundary[2],
+                'y2' => $boundary[3],
+                'ids' => array_merge($ids, ['']),
+                'precision' => $precision
+            ], [
+                'ids' => Connection::PARAM_STR_ARRAY
+            ]);
 
-        $points = $connection->executeQuery($query2, [
-            'x1' => $boundary[0],
-            'y1' => $boundary[1],
-            'x2' => $boundary[2],
-            'y2' => $boundary[3],
-            'ids' => array_merge($ids, ['']),
-            'precision' => $precision
-        ], [
-            'ids' => Connection::PARAM_STR_ARRAY
-        ])->fetchAll();
+        if (count($categories)) {
+            $q2->andWhere('category_id IN (:categories)')
+                ->setParameter('categories', $categories, Connection::PARAM_STR_ARRAY);
+        }
+
+
+        $points = $q2->execute()->fetchAll();
 
         $icon = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+Cjxzdmcgd2lkdGg9IjIyIiBoZWlnaHQ9IjI4IiB2aWV3Qm94PSIwIDAgMjIgMjgiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik02LjE0ODY1IDE1LjcxNTZDNS43MzUzOSAxNS43MTU2IDUuNDAwMzkgMTYuMDUwNiA1LjQwMDM5IDE2LjQ2MzlWMjIuMzM3NEM1LjQwMDM5IDIyLjc1MDYgNS43MzUzOSAyMy4wODU2IDYuMTQ4NjUgMjMuMDg1NkM2LjU2MTkyIDIzLjA4NTYgNi44OTY5MiAyMi43NTA2IDYuODk2OTIgMjIuMzM3NFYxNi40NjM5QzYuODk2OTIgMTYuMDUwNiA2LjU2MTkyIDE1LjcxNTYgNi4xNDg2NSAxNS43MTU2WiIgZmlsbD0id2hpdGUiLz4KPHBhdGggZD0iTTExLjgwOTkgMTUuNzE1NkMxMS4zOTY2IDE1LjcxNTYgMTEuMDYxNiAxNi4wNTA2IDExLjA2MTYgMTYuNDYzOVYyMi4zMzc0QzExLjA2MTYgMjIuNzUwNiAxMS4zOTY2IDIzLjA4NTYgMTEuODA5OSAyMy4wODU2QzEyLjIyMzEgMjMuMDg1NiAxMi41NTgxIDIyLjc1MDYgMTIuNTU4MSAyMi4zMzc0VjE2LjQ2MzlDMTIuNTU4MSAxNi4wNTA2IDEyLjIyMzEgMTUuNzE1NiAxMS44MDk5IDE1LjcxNTZaIiBmaWxsPSJ3aGl0ZSIvPgo8cGF0aCBkPSJNMTguMzAwNCAxMC4xMDE5SDE3LjQzNDFWOS4yNTAyNkMxNy43NjY2IDguNzAwOTUgMTcuOTU4MyA4LjA1NzM3IDE3Ljk1ODMgNy4zNjk3MUMxNy45NTgzIDUuNTE2NCAxNi41NjY5IDMuOTgyMDEgMTQuNzczOSAzLjc1NjExQzE0LjQzOSAyLjA5MTE0IDEyLjk2NTEgMC44MzMzNzQgMTEuMjAzMSAwLjgzMzM3NEMxMC4xNzkgMC44MzMzNzQgOS4yMTk4NSAxLjI1ODYxIDguNTM2NzUgMS45OTQ1M0M4LjE4Njc5IDEuODg1NjYgNy44MjAyOSAxLjgyOTI0IDcuNDUzMjYgMS44MjkyNEM2LjA5MDgyIDEuODI5MjQgNC44NTIyMSAyLjYwMTQ1IDQuMjMxMDggMy43NzUxMUM0LjAzNzI4IDMuNzQzMzggMy44NDA2MyAzLjcyNzQ1IDMuNjQyMTkgMy43Mjc0NUMxLjYzMzg0IDMuNzI3NDUgMCA1LjM2MTM2IDAgNy4zNjk3MUMwIDguMDU3MzcgMC4xOTE3MDYgOC43MDA5NSAwLjUyNDIzNiA5LjI1MDI2VjIzLjAxMDRDMC41MjQyMzYgMjUuNDg2IDIuNTM4MjcgMjcuNSA1LjAxMzg0IDI3LjVIMTIuOTQ0NkMxNS4yMzAyIDI3LjUgMTcuMTIxMiAyNS43ODI3IDE3LjM5OCAyMy41NzA3SDE4LjMwMDRDMTkuOTQ5MiAyMy41NzA3IDIxLjI5MDcgMjIuMjI5MyAyMS4yOTA3IDIwLjU4MDRWMTMuMDkyM0MyMS4yOTA3IDExLjQ0MzQgMTkuOTQ5MiAxMC4xMDE5IDE4LjMwMDQgMTAuMTAxOVpNMy42NDIxOSA1LjIyMzk4QzMuOTA0ODMgNS4yMjM5OCA0LjE2MjE2IDUuMjcxMzUgNC40MDcwNyA1LjM2NDhDNC42MDI1MiA1LjQzOTQxIDQuODIwMjYgNS40Mjg3MSA1LjAwNzU1IDUuMzM1NEM1LjE5NDc3IDUuMjQyMDkgNS4zMzQ0NyA1LjA3NDYzIDUuMzkyNjEgNC44NzM3MkM1LjY1NjMgMy45NjIzMyA2LjUwMzcxIDMuMzI1NzggNy40NTMyNiAzLjMyNTc4QzcuNzk4MzYgMy4zMjU3OCA4LjEyODQyIDMuNDA1NzcgOC40MzQzOSAzLjU2MzVDOC43NzU2OCAzLjczOTM0IDkuMTk0NzEgMy42Mjg1MyA5LjQwNDIyIDMuMzA2NzdDOS44MDI2NyAyLjY5NTA2IDEwLjQ3NTEgMi4zMjk5MSAxMS4yMDMxIDIuMzI5OTFDMTIuMzgzNiAyLjMyOTkxIDEzLjM0NDQgMy4yODgyMSAxMy4zNDg4IDQuNDY3NzFDMTMuMzQ4NiA0LjQ3Mzg0IDEzLjM0ODYgNC40ODAwNSAxMy4zNDg2IDQuNDg0ODRDMTMuMzQ4NiA0LjY5MzA5IDEzLjQzNTQgNC44OTE5IDEzLjU4ODEgNS4wMzM1NUMxMy43NDA4IDUuMTc1MTIgMTMuOTQ1NCA1LjI0NjczIDE0LjE1MzIgNS4yMzEwMUMxNC4yMTU2IDUuMjI2MyAxNC4yNjg5IDUuMjIzOTggMTQuMzE2MSA1LjIyMzk4QzE1LjQ5OTIgNS4yMjM5OCAxNi40NjE4IDYuMTg2NTUgMTYuNDYxOCA3LjM2OTcxQzE2LjQ2MTggOC41NTI4NyAxNS40OTkyIDkuNTE1NDQgMTQuMzE2MSA5LjUxNTQ0SDcuOTYyOTFDNy41NDk2NCA5LjUxNTQ0IDcuMjE0NjQgOS44NTA0NCA3LjIxNDY0IDEwLjI2MzdWMTMuMDA4N0M3LjIxNDY0IDEzLjQxOTUgNi44ODA1NCAxMy43NTM2IDYuNDY5ODkgMTMuNzUzNkM2LjA1OTE3IDEzLjc1MzYgNS43MjQ5OSAxMy40MTk1IDUuNzI0OTkgMTMuMDA4N1YxMC4yNjM3QzUuNzI0OTkgOS44NTA0NCA1LjM4OTk5IDkuNTE1NDQgNC45NzY3MiA5LjUxNTQ0SDMuNjQyMTlDMi40NTkwMyA5LjUxNTQ0IDEuNDk2NTMgOC41NTI4NyAxLjQ5NjUzIDcuMzY5NzFDMS40OTY1MyA2LjE4NjU1IDIuNDU5MDMgNS4yMjM5OCAzLjY0MjE5IDUuMjIzOThaTTE1LjkzNzYgMjMuMDEwNEMxNS45Mzc2IDI0LjY2MDggMTQuNTk0OSAyNi4wMDM1IDEyLjk0NDYgMjYuMDAzNUg1LjAxMzc2QzMuMzYzMzkgMjYuMDAzNSAyLjAyMDcgMjQuNjYwOCAyLjAyMDcgMjMuMDEwNFYxMC42Mjk4QzIuNTA5MzkgMTAuODczOCAzLjA1OTgxIDExLjAxMTkgMy42NDIxMiAxMS4wMTE5SDQuMjI4NDZWMTMuMDA4N0M0LjIyODQ2IDE0LjI0NDYgNS4yMzM5OCAxNS4yNSA2LjQ2OTg5IDE1LjI1QzcuNzA1NzMgMTUuMjUgOC43MTExOCAxNC4yNDQ1IDguNzExMTggMTMuMDA4N1YxMS4wMTE5SDE0LjMxNjFDMTQuODk4NSAxMS4wMTE5IDE1LjQ0ODkgMTAuODczOCAxNS45Mzc2IDEwLjYyOThWMjMuMDEwNEgxNS45Mzc2Wk0xOS43OTQyIDIwLjU4MDRDMTkuNzk0MiAyMS40MDQxIDE5LjEyNDEgMjIuMDc0MiAxOC4zMDA1IDIyLjA3NDJIMTcuNDM0MlYxMS41OTg1SDE4LjMwMDVDMTkuMTI0MSAxMS41OTg1IDE5Ljc5NDIgMTIuMjY4NiAxOS43OTQyIDEzLjA5MjNWMjAuNTgwNFoiIGZpbGw9IndoaXRlIi8+Cjwvc3ZnPgo=";
 
@@ -164,10 +178,8 @@ final class ObjectsApiController extends AbstractController
         }, $clusters);
 
 
-        $clusters = [
-            'type' => 'FeatureCollection',
-            'features' => array_merge($clustersPrepared, $pointsPrepared)
-        ];
+        $clusters = ['type' => 'FeatureCollection',
+            'features' => array_merge($clustersPrepared, $pointsPrepared)];
 
         $response = new JsonResponse($clusters);
 
