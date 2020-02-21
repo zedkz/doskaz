1af62240614cb256f8c945d79766b8fb35c269ab
smolin <smolin9000@gmail.com>
2020-02-21 22:03:52 +0600

    Correct icon shape coordinates

--- backend/src/Objects/ObjectsApiController.php
+++ backend/src/Objects/ObjectsApiController.php
@@ -22,15 +22,6 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 final class ObjectsApiController extends AbstractController
 {
-    private $pointTemplate = <<<TEMPLATE
-          <div style="border: none;  display: flex; width: 50px; height: 61px; padding-bottom: 11px; justify-content: center; align-items: center;">
-              <svg width="50" height="61" viewBox="0 0 50 61" fill="none" xmlns="http://www.w3.org/2000/svg" style="position: absolute; top: 0; left: 0; z-index: 0;">
-                  <path d="M50 0H0V50H14.6667L25 60.3333L35.3333 50H50V0Z" fill="%color"/>
-              </svg>
-              <i class='fa fa-2x %icon' style='z-index: 1; color: white'></i>
-          </div>
-TEMPLATE;
-
     /**
      * @Route(path="/ymaps", methods={"GET"})
      * @param Request $request
@@ -174,7 +165,7 @@ TEMPLATE;
                     'iconShape' => [
                         'type' => 'Rectangle',
                         'coordinates' => [
-                            [-25, -25], [25, 25]
+                            [-25, -60], [25, 0]
                         ]
                     ]
                 ],
