6ee59a2f7320ead131996fbea31923a9645414ef
Myrzagali <mirzagaly@gmail.com>
2020-04-09 13:44:47 +0600

    DPVL-460

---
+++ frontend/src/components/ViFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="vi__footer --bcolor">
+        <a href="" class="vi__footer-link">Помощь</a>
+        <a href="" class="vi__footer-link">О проекте</a>
+        <a href="" class="vi__footer-link">Блог</a>
+        <a href="" class="vi__footer-link">Контакты</a>
+    </div>
+</template>
+<style lang="scss">
+
+    .vi {
+        &__footer {
+            border-top: 3px solid;
+            padding: 40px 0;
+            display: flex;
+            &-link {
+                line-height: 24px;
+                margin: 0 0 0 60px;
+                &:first-child {
+                    margin: 0;
+                }
+            }
+        }
+    }
+</style>
