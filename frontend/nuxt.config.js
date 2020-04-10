d56b84ca5cabffa4f445b988c6a5f8e092bc3bad
smolin <smolin9000@gmail.com>
2020-04-10 10:37:09 +0600

    Formatting

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -54,11 +54,7 @@ export default {
             {type: "text/javascript", src: "https://ulogin.ru/js/ulogin.js"}
         ],
         link: [
-            {
-                rel: 'icon',
-                type: 'image/png',
-                href: '/favicon.png',
-            },
-        ],
-    },
+            {rel: 'icon', type: 'image/png', href: '/favicon.png'}
+        ]
+    }
 }
