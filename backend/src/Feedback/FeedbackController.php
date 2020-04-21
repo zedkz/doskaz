1b04a552c377864eeefa929ad91ca705a853a5f0
smolin <smolin9000@gmail.com>
2020-04-21 19:05:26 +0600

    Feedback api docs

--- backend/src/Feedback/FeedbackController.php
+++ backend/src/Feedback/FeedbackController.php
@@ -5,6 +5,12 @@ namespace App\Feedback;
 
 
 use App\Infrastructure\Doctrine\Flusher;
+use OpenApi\Annotations\Items;
+use OpenApi\Annotations\JsonContent;
+use OpenApi\Annotations\Post;
+use OpenApi\Annotations\Property;
+use OpenApi\Annotations\RequestBody;
+use OpenApi\Annotations\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +24,36 @@ class FeedbackController extends AbstractController
      * @param FeedbackData $feedbackData
      * @param FeedbackRepository $feedbackRepository
      * @param Flusher $flusher
+     * @Post(
+     *     path="/api/feedback",
+     *     tags={"Обратная связь"},
+     *     summary="Отправка обратной связи",
+     *     @RequestBody(
+     *         @JsonContent(
+     *             @Property(property="name", type="string"),
+     *             @Property(property="email", type="string"),
+     *             @Property(property="text", type="string"),
+     *         )
+     *     ),
+     *     @Response(response="204", description=""),
+     *     @Response(
+     *         response=400,
+     *         description="Validation Failed",
+     *         @JsonContent(
+     *             @Property(property="message", type="string", example="Validation Failed"),
+     *             @Property(property="code", type="number", example=400),
+     *             @Property(
+     *                 property="errors",
+     *                 type="array",
+     *                 @Items(
+     *                     type="object",
+     *                     @Property(property="property", type="string", description="Имя свойства"),
+     *                     @Property(property="message", type="string", description="Текст ошибки"),
+     *                 )
+     *             )
+     *         )
+     *     ),
+     * )
      */
     public function create(FeedbackData $feedbackData, FeedbackRepository $feedbackRepository, Flusher $flusher)
     {
