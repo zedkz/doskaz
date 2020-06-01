5ca61c19abb3df09ce9b198404abbd4bc601e2a9
smolin <smolin9000@gmail.com>
2020-06-01 19:43:50 +0600

    DPVL-452

--- backend/src/Objects/Zone/Middle/Navigation.php
+++ backend/src/Objects/Zone/Middle/Navigation.php
@@ -3,7 +3,6 @@
 
 namespace App\Objects\Zone\Middle;
 
-use App\Objects\AccessibilityScoreBuilder;
 use App\Objects\Adding\AccessibilityScore;
 use App\Objects\Adding\Attribute;
 use App\Objects\AttributesConfiguration;
@@ -22,14 +21,16 @@ class Navigation extends Zone
             return AccessibilityScore::fullAccessible();
         }
         if ($this->isMatchesAll(Attribute::unknown())) {
-            return AccessibilityScore::notAccessible();
+            return AccessibilityScore::unknown();
         }
         if ($this->isMatchesAll(Attribute::notProvided())) {
             return AccessibilityScore::notProvided();
         }
 
-        $builder = AccessibilityScoreBuilder::initPartialAccessible();
+        if ($this->isMatches([5, 6, 7], Attribute::no())) {
+            return AccessibilityScore::notAccessible();
+        }
 
-        return $builder->build();
+        return AccessibilityScore::partialAccessible();
     }
 }
