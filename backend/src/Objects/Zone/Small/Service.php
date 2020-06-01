45b42a3d3fcf0a1f7c18954c409fa96b323666d6
smolin <smolin9000@gmail.com>
2020-06-01 19:30:15 +0600

    DPVL-452

--- backend/src/Objects/Zone/Small/Service.php
+++ backend/src/Objects/Zone/Small/Service.php
@@ -5,36 +5,28 @@ namespace App\Objects\Zone\Small;
 
 use App\Objects\Adding\AccessibilityScore;
 use App\Objects\Adding\Attribute;
+use App\Objects\AttributesConfiguration;
 use App\Objects\Zone;
 
 class Service extends Zone
 {
     protected static function attributesKeys(): array
     {
-        return ['attribute1000'];
+        return AttributesConfiguration::getAttributesKeysForFormAndZone('small', 'service');
     }
 
     public function calculateScore(): AccessibilityScore
     {
-        /**
-         * @var $attribute Attribute
-         */
-        $attribute = $this->attributes->get('attribute1000');
-
-        if ($attribute->isEqualsTo(Attribute::notProvided())) {
+        if ($this->isMatchesAll(Attribute::yes())) {
+            return AccessibilityScore::fullAccessible();
+        }
+        if ($this->isMatchesAll(Attribute::unknown())) {
+            return AccessibilityScore::unknown();
+        }
+        if ($this->isMatchesAll(Attribute::notProvided())) {
             return AccessibilityScore::notProvided();
         }
 
-        $movement = AccessibilityScore::SCORE_NOT_ACCESSIBLE;
-        $limb = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        $vision = AccessibilityScore::SCORE_NOT_ACCESSIBLE;
-        $hearing = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        $intellectual = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-
-        if ($attribute->isEqualsTo(Attribute::yes())) {
-            $movement = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-            $vision = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        }
-        return AccessibilityScore::new($movement, $limb, $vision, $hearing, $intellectual);
+        return AccessibilityScore::partialAccessible();
     }
 }
