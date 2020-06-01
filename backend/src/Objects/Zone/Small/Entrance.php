aac8800da9da29c89bf23c848124b2a41a2a160c
smolin <smolin9000@gmail.com>
2020-06-01 19:20:44 +0600

    DPVL-452

--- backend/src/Objects/Zone/Small/Entrance.php
+++ backend/src/Objects/Zone/Small/Entrance.php
@@ -3,52 +3,46 @@
 
 namespace App\Objects\Zone\Small;
 
+use App\Objects\AccessibilityScoreBuilder;
 use App\Objects\Adding\AccessibilityScore;
 use App\Objects\Adding\Attribute;
+use App\Objects\AttributesConfiguration;
 use App\Objects\Zone;
 
 class Entrance extends Zone
 {
     protected static function attributesKeys(): array
     {
-        return ['attribute1', 'attribute1000', 'attribute1001', 'attribute30', 'attribute31', 'attribute1002'];
+        return AttributesConfiguration::getAttributesKeysForFormAndZone('small', 'entrance');
     }
 
     public function calculateScore(): AccessibilityScore
     {
+        if ($this->isMatchesAll(Attribute::yes())) {
+            return AccessibilityScore::fullAccessible();
+        }
+        if ($this->isMatchesAll(Attribute::unknown())) {
+            return AccessibilityScore::unknown();
+        }
         if ($this->isMatchesAll(Attribute::notProvided())) {
             return AccessibilityScore::notProvided();
         }
 
-        $movement = AccessibilityScore::SCORE_NOT_ACCESSIBLE;
-        $limb = AccessibilityScore::SCORE_NOT_ACCESSIBLE;
-        $vision = AccessibilityScore::SCORE_NOT_ACCESSIBLE;
-        $hearing = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        $intellectual = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
+        $builder = AccessibilityScoreBuilder::initPartialAccessible();
 
-        if ($this->isMatches([1], Attribute::yes())) {
-            $limb = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-            $vision = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
+        if ($this->isMatches([1], Attribute::yes()) || $this->isMatches([30, 31, 1002], Attribute::yes())) {
+            $builder->withVisionFullAccessible()
+                ->withLimbFullAccessible()
+                ->withHearingFullAccessible()
+                ->withIntellectualFullAccessible();
         }
 
-        if ($this->isMatches([1, 31, 1002], Attribute::yes()) || $this->isMatches([1000, 1001, 30, 31, 1002], Attribute::yes())) {
-            $movement = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        } elseif ($this->isMatchesPartial([1, 31, 1002], Attribute::yes()) || $this->isMatchesPartial([1000, 1001, 30, 31, 1002], Attribute::yes())) {
-            $movement = AccessibilityScore::SCORE_PARTIAL_ACCESSIBLE;
+        if ($this->isMatches([1, 30, 31], Attribute::yes()) || $this->isMatches([1000, 1001, 30, 31, 1002], Attribute::yes())) {
+            $builder->withMovementFullAccessible();
+        } elseif ($this->isMatches([1, 1000, 1001], Attribute::no())) {
+            $builder->withMovementNotAccessible();
         }
 
-        if ($this->isMatches([1000, 1001, 31, 1002], Attribute::yes())) {
-            $limb = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        } elseif ($this->isMatchesPartial([1000, 1001, 31, 1002], Attribute::yes())) {
-            $limb = AccessibilityScore::SCORE_PARTIAL_ACCESSIBLE;
-        }
-
-        if ($this->isMatches([31, 1002], Attribute::yes())) {
-            $vision = AccessibilityScore::SCORE_FULL_ACCESSIBLE;
-        } elseif ($this->isMatchesPartial([31, 1002], Attribute::yes())) {
-            $vision = AccessibilityScore::SCORE_PARTIAL_ACCESSIBLE;
-        }
-
-        return AccessibilityScore::new($movement, $limb, $vision, $hearing, $intellectual);
+        return $builder->build();
     }
 }
