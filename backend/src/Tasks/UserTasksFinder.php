a5b2cffdd0f1835905b17d36cd0dce783d884be2
smolin <smolin9000@gmail.com>
2020-06-11 11:59:12 +0600

    Current timestamp

--- backend/src/Tasks/UserTasksFinder.php
+++ backend/src/Tasks/UserTasksFinder.php
@@ -38,7 +38,7 @@ class UserTasksFinder
 
         if ($currentTask) {
             $query  = $query. "
-                union all select null as completed_at, :currentTaskTitle as type, :currentTaskPoints as points, CURRENT_TIMESTAMP as created_at, 1 as priority
+                union all select null as completed_at, :currentTaskTitle as type, :currentTaskPoints as points, CURRENT_TIMESTAMP(0) as created_at, 1 as priority
             ";
         }
 
