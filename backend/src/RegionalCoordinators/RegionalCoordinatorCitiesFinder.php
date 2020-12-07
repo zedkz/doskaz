9e1d7410bf16f2ce0daa06bdfcf75cbd2cef1453
smolin <smolin9000@gmail.com>
2020-12-07 12:07:49 +0600

    Regional coordinator city fix

--- backend/src/RegionalCoordinators/RegionalCoordinatorCitiesFinder.php
+++ backend/src/RegionalCoordinators/RegionalCoordinatorCitiesFinder.php
@@ -20,9 +20,8 @@ class RegionalCoordinatorCitiesFinder
     public function find(int $userId): array
     {
         return $this->connection->createQueryBuilder()
-            ->select('cities_geometry.id')
+            ->select('cities.id')
             ->from('cities')
-            ->join('cities', 'cities_geometry', 'cities_geometry', 'st_intersects(cities.bbox, cities_geometry.geometry)')
             ->andWhere('cities.id in (select jsonb_array_elements_text(cities)::int from regional_coordinators where user_id = :userId)')
             ->setParameter('userId', $userId)
             ->execute()
