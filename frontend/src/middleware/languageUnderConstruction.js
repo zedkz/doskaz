70e8cc647cb43da61bdd8d195ba6a1c2bbb683f1
smolin <smolin9000@gmail.com>
2021-01-15 14:00:37 +0600

    Update translations

--- frontend/src/middleware/languageUnderConstruction.js
+++
@@ -1,5 +0,0 @@
-export default function ({app, redirect, route}) {
-    if (route.fullPath !== '/kz/underConstruction' && route.fullPath.startsWith('/kz')) {
-        return redirect(app.localePath('/underConstruction', 'kz'))
-    }
-}
