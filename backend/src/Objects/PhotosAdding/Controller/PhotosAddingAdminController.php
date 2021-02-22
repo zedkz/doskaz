21e10559e36cefc3bb2664b68fad91765b8d2d6c
smolin <smolin9000@gmail.com>
2021-02-22 19:12:42 +0600

    feat: Photos adding requests regional coordinators filter

--- backend/src/Objects/PhotosAdding/Controller/PhotosAddingAdminController.php
+++ backend/src/Objects/PhotosAdding/Controller/PhotosAddingAdminController.php
@@ -3,9 +3,12 @@
 
 namespace App\Objects\PhotosAdding\Controller;
 
+use App\AdminpanelPermissions\AdminpanelPermission;
 use App\Infrastructure\Doctrine\Flusher;
 use App\Infrastructure\FileReferenceCollection;
 use App\Objects\PhotosAdding\Entity\PhotosAddingRequest;
+use App\RegionalCoordinators\RegionalCoordinatorCitiesFinder;
+use App\RegionalCoordinators\RegionalCoordinatorRepository;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,7 +20,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route(path="/api/admin/photosAdding")
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("objects_access")
  */
 class PhotosAddingAdminController extends AbstractController
 {
@@ -25,14 +28,30 @@ class PhotosAddingAdminController extends AbstractController
      * @Route(methods={"GET"})
      * @param Request $request
      * @param Connection $connection
+     * @param RegionalCoordinatorCitiesFinder $coordinatorCitiesFinder
+     * @param RegionalCoordinatorRepository $regionalCoordinatorRepository
      * @return array
      * @throws Exception
      */
-    public function list(Request $request, Connection $connection): array
+    public function list(Request $request, Connection $connection, RegionalCoordinatorCitiesFinder $coordinatorCitiesFinder, RegionalCoordinatorRepository $regionalCoordinatorRepository): array
     {
         $query = $connection->createQueryBuilder()
             ->from('object_photos_adding_requests', 'r')
-            ->andWhere("r.status != 'approved'");
+            ->andWhere("r.status != 'approved'")
+            ->join('r', 'objects', 'objects', 'objects.id = r.object_id');
+
+        if ($regionalCoordinatorRepository->findByUserId($this->getUser()->id())) {
+            $cities = $coordinatorCitiesFinder->find($this->getUser()->id());
+            $query->andWhere('
+                EXISTS(
+                    SELECT 1
+                    FROM cities_geometry
+                    WHERE cities_geometry.id IN (:cities)
+                    AND ST_Contains(cities_geometry.geometry, objects.point_value::geometry)
+                    LIMIT 1
+                )
+            ')->setParameter('cities', $cities, Connection::PARAM_INT_ARRAY);
+        }
 
         $items = (clone $query)
             ->addSelect('r.id')
