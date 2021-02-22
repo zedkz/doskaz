25a4c9f1f112d6e3593ddf9643ad27c1a88fcd4c
smolin <smolin9000@gmail.com>
2021-02-22 14:40:33 +0600

    docs: Api docs

--- backend/src/Objects/PhotosAdding/Controller/PhotosAddingController.php
+++ backend/src/Objects/PhotosAdding/Controller/PhotosAddingController.php
@@ -9,6 +9,14 @@ use App\Objects\PhotosAdding\Entity\PhotosAddingRequest;
 use App\Objects\PhotosAdding\PhotosAddingData;
 use App\Objects\PhotosAdding\Repository\PhotosAddingRequestRepository;
 use App\Users\Security\AuthenticatedUser;
+use OpenApi\Annotations\Items;
+use OpenApi\Annotations\JsonContent;
+use OpenApi\Annotations\Parameter;
+use OpenApi\Annotations\Post;
+use OpenApi\Annotations\Property;
+use OpenApi\Annotations\RequestBody;
+use OpenApi\Annotations\Response;
+use OpenApi\Annotations\Schema;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,6 +31,23 @@ class PhotosAddingController extends AbstractController
     /**
      * @Entity("object", expr="repository.findOneBy({""id"": id, ""deletedAt"": null})")
      * @Route(path="/addPhotos", methods={"POST"})
+     * @Post(
+     *     path="/api/objects/{id}/addPhotos",
+     *     summary="Создать запрос на добавление фото к объекту",
+     *     tags={"Объекты"},
+     *     security={{"clientAuth": {}}},
+     *     @Parameter(name="id", in="path", required=true, description="id объекта", @Schema(type="integer"), example=172709),
+     *     @RequestBody(
+     *         @JsonContent(
+     *             @Property(property="photos", type="array", @Items(type="string"), description="Ссылки на фото", example={}),
+     *         )
+     *     ),
+     *     @Response(response=204, description=""),
+     *     @Response(response=400, description=""),
+     *     @Response(response=401, description=""),
+     *     @Response(response=404, description="Объект не найден"),
+     *
+     * )
      * @param PhotosAddingData $photosAddingData
      * @param MapObject $object
      * @param PhotosAddingRequestRepository $photosAddingRequestRepository
